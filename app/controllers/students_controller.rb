17760f2404cc4da61513d67e6da3ae2d5d6cfc26
Zachary Soliman <zachsoliman1@gmail.com>
2022-08-31 21:53:31 -0400

    complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,9 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    s = Student.find_by!(id: params[:id])
+    render json: s
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/students', to: 'students#index'
+  get '/students/:id', to: 'students#show'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
 end
